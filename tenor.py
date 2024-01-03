67af2b906e28ccbe0f9d8fa49e2403a09f07d04f
github.4o <githubmail.4o@ya.ru>
2024-01-03 23:26:57 +0700

    fix yes/no exceptions

--- tenor.py
+++ tenor.py
@@ -87,7 +87,7 @@ class tenor_cog(commands.Cog):
             if len(message.mentions) == 0 and not message.reference:
                 if not txt[1].isalnum():
                     return
-                if txt in ['yes', 'yep', 'yup', 'nop', 'no', 'nope']:
+                if txt.lower() in ['yes', 'yep', 'yup', 'nop', 'no', 'nope']:
                     return
                 if txt.startswith('http'):
                     return
